a4c5320c135c14608dc99b4eea19f8f51e04c705
Charan-3132 <np3458@srmist.edu.in>
2026-04-07 10:55:14 +0530

    UC19: Binary search for bogie ID with sorting

--- src/com/train/app/TrainConsistManagementApp.java
+++ src/com/train/app/TrainConsistManagementApp.java
@@ -4,12 +4,27 @@ import java.util.Arrays;
 
 public class TrainConsistManagementApp {
 
-    // 🔥 Linear Search Method
-    public static boolean linearSearch(String[] bogies, String key) {
+    // 🔥 Binary Search Method
+    public static boolean binarySearch(String[] bogies, String key) {
 
-        for (String id : bogies) {
-            if (id.equals(key)) {
+        // ensure sorted
+        Arrays.sort(bogies);
+
+        int low = 0;
+        int high = bogies.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            int cmp = key.compareTo(bogies[mid]);
+
+            if (cmp == 0) {
                 return true; // found
+            } else if (cmp < 0) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
@@ -19,17 +34,16 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("====================================");
-        System.out.println("UC18 - Linear Search for Bogie ID");
+        System.out.println("UC19 - Binary Search for Bogie ID");
         System.out.println("====================================\n");
 
         String[] bogieIds = {"BG101","BG205","BG309","BG412","BG550"};
 
         String searchKey = "BG309";
 
-        System.out.println("Bogie IDs: " + Arrays.toString(bogieIds));
         System.out.println("Searching for: " + searchKey);
 
-        boolean found = linearSearch(bogieIds, searchKey);
+        boolean found = binarySearch(bogieIds, searchKey);
 
         if (found) {
             System.out.println("Bogie Found!");
